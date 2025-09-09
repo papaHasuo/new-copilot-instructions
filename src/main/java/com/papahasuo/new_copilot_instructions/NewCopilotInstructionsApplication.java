ea48c2d38e9c20d52a01522df44445b6627c0f50
papa_hasuo <papa_hasuo@outlook.jp>
2025-09-09 18:09:40 +0900

    init commit

---
+++ src/main/java/com/papahasuo/new_copilot_instructions/NewCopilotInstructionsApplication.java
@@ -0,0 +1,13 @@
+package com.papahasuo.new_copilot_instructions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCopilotInstructionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCopilotInstructionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papahasuo/new_copilot_instructions/NewCopilotInstructionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papahasuo.new_copilot_instructions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCopilotInstructionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
